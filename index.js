8bf531d0c53d8acbbe0f3976a70ebbc0f2b825a7
欧阳城城 <3080690467@qq.com>
2026-03-16 19:29:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const sequelize = new Sequelize(
   }
 );
 
-const User = sequelize.define('User', {
+const User = sequelize.define('user', {
   openid: { type: DataTypes.STRING, allowNull: false, unique: true },
   nickName: { type: DataTypes.STRING, defaultValue: '微信用户' }
 });
